dfe1710880d5feec77ee9ca15c29f647bc81e480
sebastianjul97 <sebastian.corderoju@amigo.edu.co>
2022-10-04 22:50:06 -0500

    hpta si se pudo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = hola si si por favor;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = jonathan loca;
 
 // Crea una variable booleana:
 const nuevoBool = null;
